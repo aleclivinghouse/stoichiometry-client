2ffd421518a62bca27ad254af7aee15bc08b0366
aleclivinghouse <aleclivinghouse@yahoo.com>
2019-03-05 14:55:27 -0800

    changed button

--- src/components/registration-page.js
+++ src/components/registration-page.js
@@ -12,7 +12,6 @@ export function RegistrationPage(props) {
     }
     return (
         <div className="home">
-            <h2>Register for Stoichiometry Calculator</h2>
             <RegistrationForm />
             <Link to="/">Login</Link>
         </div>
